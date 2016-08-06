8e97376c19a3833bdeacf87fb51a4972db1f5313
jciancio <jciancio7@gmail.com>
2016-08-06 00:17:11 -0400

    set up chatrooms

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :chatroom_users
   has_many :users, through: :chatroom_users
   has_many :messages
+
+  def recent_messages
+    messages.order(created_at: :desc).limit(100).reverse
+  end
 end
